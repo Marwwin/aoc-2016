6db2209890d1506084a2453e810a62eb58df866d
Marwwin <markus.kankkonen@protonmail.com>
2024-07-07 22:46:05 +0300

    day 8 done

--- src/aoc_2016/days/day8.clj
+++ src/aoc_2016/days/day8.clj
@@ -1,102 +1,89 @@
 (ns aoc-2016.days.day8
   (:require [clojure.string :as str]
-            [clojure.math :as math]
             [aoc-2016.aoc-utils :as utils]))
 
-(declare part1 new-screen print-screen set-pixel rect screen-at rotate get-row get-col shift index-to-pos)
+(declare part1 new-screen print-screen update-screen rect rotate get-row get-col shift-vec count-lit)
 
 (defn solve []
   (let [input (utils/load-input "day8")
-        screen (new-screen 50 6)]
-    (part1 input screen)))
+        screen (new-screen 50 6)
+        res (part1 input screen)]
+    (print-screen res)
+    {:part1 (count-lit res) :part2 "see above"}))
 
-(defn part1 [inst screen]
-  (loop [instrs inst
+(defn part1 [instructions screen]
+  (loop [instructions instructions
          screen screen]
-    (println "recur" (first instrs))
-    (if (empty? instrs)
-      (print-screen screen)
-      (let [instruction (str/split (first instrs)  #"\s+")
-            scr (case (first instruction)
-                  "rect" (rect screen (rest instruction))
-                  "rotate" (rotate screen (rest instruction)))]
-        (println "hepp" instruction)
-        (recur (rest instrs) scr)))))
+    (if (empty? instructions)
+      screen
+      (let [[instruction & args] (str/split (first instructions)  #"\s+")]
+        (recur (rest instructions) (case  instruction
+                                     "rect" (rect screen args)
+                                     "rotate" (rotate screen args)))))))
 
-(defn rect [screen [instr]]
-  (println "h" (screen :pixels))
-  (let [[x y] (str/split instr #"x")
-        to-add (flatten (map (fn [row-i]
-                               (map (fn [i] (+ i (* row-i (screen :width))))
-                                    (range (Integer/parseInt x))))
-                             (range (Integer/parseInt y))))
-        newp (reduce (fn [acc idx]
-                       (assoc acc idx "x"))
-                     (screen :pixels)
-                     to-add)]
-    (println newp)
+(defn rect [screen [size]]
+  (let [[x y] (str/split size #"x")
+        ys (range (Integer/parseInt y))
+        xs (range (Integer/parseInt x))
+        width (screen :width)
+        to-add (->> ys (mapcat (fn [y-index]
+                                 (->> xs
+                                      (map
+                                       (fn [x-index]
+                                         [(+ x-index (* y-index width)) "x"]))))))
+        newp (update-screen screen to-add)]
     (assoc screen :pixels newp)))
 
+(defn update-screen
+  [screen to-add]
+  (reduce
+   (fn [acc [idx ch]]
+     (assoc acc idx ch))
+   (screen :pixels)
+   to-add))
+
 (defn rotate [screen [row-col i _ n]]
-  (println i n)
-  (let [i (Integer/parseInt (second (str/split i #"=")))]
+  (let [i (Integer/parseInt (second (str/split i #"=")))
+        n (Integer/parseInt n)]
     (assoc
      screen :pixels
-     (case row-col
-       "row" (shift (get-row screen i) n)
-       "column" (shift (get-col screen i) n)))))
+     (let [to-change (case row-col
+                       "row"  (get-row screen i n)
+                       "column"  (get-col screen i n))]
+       (update-screen screen to-change)))))
 
-; This needs to update the correct row-col in pixels 
-(defn shift [coll n]
-  (vec (concat (drop n coll) (take n coll))))
+(defn get-col [screen i n]
+  (let  [symbols (mapcat (fn [coll] [(nth coll i)])
+                         (partition (screen :width) (screen :pixels)))
+         indexes (map (fn [ii]  (+ i (* ii (screen :width))))
+                      (range (screen :height)))]
+    (map vector (shift-vec indexes n) symbols)))
+(get-col (rect (new-screen 5 5) ["3x2"])  1 1)
 
-(defn get-col [screen i]
-  (map
-   (fn [coll] (nth coll i))
-   (partition (screen :width) (screen :pixels))))
+(defn shift-vec [v n]
+  (let [v (vec v)]
+    (vec (concat
+          (subvec v n)
+          (subvec v 0 n)))))
 
-(defn get-row [screen i]
-  (subvec
-   (screen :pixels)
-   (* i (screen :width))
-   (* (inc i) (screen :width))))
+(defn get-row [screen i n]
+  (let [start (* i (screen :width))
+        end (* (inc i) (screen :width))]
+    (map vector
+         (shift-vec (range start end) n)
+         (subvec (screen :pixels) start end))))
 
 (defn new-screen [wide high]
   {:pixels (vec (repeat (* wide high) "."))
    :width wide
    :height high})
 
-;(defn set-pixel [screen pos value]
-;  (let [new-pixels (assoc (vec (screen :pixels)) (screen-at screen pos) value)]
-;    (assoc screen :pixels new-pixels)))
-;
-;(set-pixel (new-screen 5 5) {:x 0 :y 0} "x")
-
-;(defn index-to-pos [screen cnt]
-;  (if (or (>= cnt (screen :pixels)) (< cnt 0))
-;    (throw
-;     (ex-info (str "Value outside of screen " cnt) {:type :out-of-bound}))
-;    (let [x (mod cnt (screen :width))
-;          y (->> (/ cnt (screen :width))
-;                 math/floor
-;                 int)]
-;      {:x x :y y})))
-;
-;(defn screen-at [screen pos]
-;  (let [x (pos :x)
-;        y (pos :y)]
-;    (if (or (< x 0) (>= x (screen :width))
-;            (< y 0) (>= y (screen :height)))
-;      (throw
-;       (ex-info
-;        (str "Position outside of screen " pos)
-;        {:type :out-of-bound}))
-;      (+
-;       (* y (screen :width)) x))))
-
 (defn print-screen [screen]
   (let [s (partition (screen :width) (screen :pixels))]
     (doseq [row s]
       (println row))))
+
+(defn count-lit [screen]
+  (count (filter (fn [c] (= c "x")) (screen :pixels))))
 
 (solve)
